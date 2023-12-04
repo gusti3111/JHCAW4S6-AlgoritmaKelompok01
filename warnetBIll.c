5f0277408ff73ba930239027aa8c81e073597e77
iwan mispuansyah <gusti.mispuansyah@gmail.com>
2023-12-04 11:11:00 +0700

    correction data type from double to float

--- warnetBIll.c
+++ warnetBIll.c
@@ -4,36 +4,34 @@ int main(){
     // Initialize variables
     int playTime;
     int billPerHour = 10000;
-    double totalBill = playTime * billPerHour;
-    double totalDiscount;
+    float totalBill = playTime * billPerHour;
+    float totalDiscount;
     printf("Enter the duration of playtime in hours: ");
     scanf("%d", &playTime);
 
     //conditions for total bill
     if(playTime <= 4){
         totalBill;
-    }else if(playTime > 4 && playTime <= 6){
-       double discount = 1 - 0.10;// karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
+    }
+    else if(playTime > 4 && playTime <= 6){
+       float discount = 1 - 0.10;// karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
 
          totalDiscount = playTime * billPerHour * discount;
     }else if(playTime > 6 && playTime <= 8){
-        double discount = 1 - 0.15; // karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
+        float discount = 1 - 0.15; // karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
 
          totalDiscount = playTime * billPerHour * discount;
 
     }else if(playTime > 8  &&  playTime <= 9 ){
-        double discount = 1- 0.20; // karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
+        float discount = 1- 0.20; // karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
         totalDiscount = playTime * billPerHour * discount;
     }else if(playTime >9) {
-         double discount = 1- 0.25;  // karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
+         float discount = 1- 0.25;  // karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
 
       totalDiscount = playTime * billPerHour * discount;
 
 
-    } else {
-        printf("Invalid playtime entered.\n");
-        return 1; // Terminate the program due to invalid input
-    }
+    } 
 
     printf("The total amount to be paid: Rp %.2f\n", totalDiscount);
     return 0;
