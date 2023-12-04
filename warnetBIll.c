945f11500755d939e2523b28efee3cfbc7e0ef68
iwan mispuansyah <gusti.mispuansyah@gmail.com>
2023-12-04 10:58:17 +0700

    create game online rental bill system

---
+++ warnetBIll.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(){
+    // Initialize variables
+    int playTime;
+    int billPerHour = 10000;
+    double totalBill = playTime * billPerHour;
+    double totalDiscount;
+    printf("Enter the duration of playtime in hours: ");
+    scanf("%d", &playTime);
+
+    //conditions for total bill
+    if(playTime <= 4){
+    }
+    if(playTime > 4 && playTime <= 6){
+       double discount = 1 - 0.10;// karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
+
+         totalDiscount = playTime * billPerHour * discount;
+    }else if(playTime > 6 && playTime <= 8){
+        double discount = 1 - 0.15; // karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
+
+         totalDiscount = playTime * billPerHour * discount;
+
+    }else if(playTime > 8  &&  playTime <= 9 ){
+        double discount = 1- 0.20; // karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
+        totalDiscount = playTime * billPerHour * discount;
+    }else if(playTime >9) {
+         double discount = 1- 0.25;  // karna discount 15% dan yang dicari costumer bayar berapa maka  1 - discount
+
+      totalDiscount = playTime * billPerHour * discount;
+
+
+    } else {
+        printf("Invalid playtime entered.\n");
+        return 1; // Terminate the program due to invalid input
+    }
+
+    printf("The total amount to be paid: Rp %.2f\n", totalDiscount);
+    return 0;
+
+}
